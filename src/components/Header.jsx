b4c45bb78ce207ebeb8aa6721348a7c394e5b182
jaaz10 <jaazespinoza@gmail.com>
2024-11-27 14:32:37 -0600

    add more work experience 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,6 +72,34 @@ function MoonIcon(props) {
   )
 }
 
+function GitHubIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"
+      />
+    </svg>
+  )
+}
+
+function LinkedInIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 01-1.548-1.549 1.548 1.548 0 111.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z" />
+    </svg>
+  )
+}
+
+function ResumeIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path d="M7 4V3h10v1h2v18H5V4h2zm4 14h2v-2h-2v2zm0-4h2v-2h-2v2zm0-4h2V8h-2v2z" />
+    </svg>
+  )
+}
+
 function MobileNavItem({ href, children }) {
   return (
     <li>
@@ -138,7 +166,7 @@ function MobileNavigation(props) {
   )
 }
 
-function NavItem({ href, children }) {
+function NavItem({ href, children, icon: Icon }) {
   let isActive = usePathname() === href
 
   return (
@@ -149,10 +177,14 @@ function NavItem({ href, children }) {
           'relative block px-3 py-2 transition',
           isActive
             ? 'text-teal-500 dark:text-teal-400'
-            : 'hover:text-teal-500 dark:hover:text-teal-400',
+            : 'hover:text-teal-500 dark:hover:text-teal-400'
         )}
       >
-        {children}
+        {Icon ? (
+          <Icon className="h-6 w-6 fill-current" />
+        ) : (
+          children
+        )}
         {isActive && (
           <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
         )}
@@ -166,10 +198,9 @@ function DesktopNavigation(props) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/">Home</NavItem>
-        <NavItem href="https://github.com/jaaz10" target="_blank">Github</NavItem>
-        <NavItem href="https://www.linkedin.com/in/jaazespinoza/" target="_blank">LinkedIn</NavItem>
-        <NavItem href="https://drive.google.com/file/d/1KR0d9ryhZ4qUF6hfk5zbJCNmJwZH6r6i/view?usp=drive_link" target="_blank">Resume</NavItem>
-        {/* <NavItem href="/uses">Uses</NavItem> */}
+        <NavItem href="https://github.com/jaaz10" icon={GitHubIcon} target="_blank" />
+        <NavItem href="https://www.linkedin.com/in/jaazespinoza/" icon={LinkedInIcon} target="_blank" />
+        <NavItem href="https://drive.google.com/file/d/1KR0d9ryhZ4qUF6hfk5zbJCNmJwZH6r6i/view?usp=drive_link" icon={ResumeIcon} target="_blank" />
       </ul>
     </nav>
   )
